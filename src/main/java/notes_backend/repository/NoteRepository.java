af3d53877021933c9d94b5d5598c03f5fae5dc21
Serj <pandastars535@gmail.com>
2025-11-03 05:43:26 +0200

    chore(noteRepositiry): added method to repository

--- src/main/java/notes_backend/repository/NoteRepository.java
+++ src/main/java/notes_backend/repository/NoteRepository.java
@@ -1,14 +1,21 @@
 package notes_backend.repository;
 
 import notes_backend.entity.note.Note;
+import notes_backend.entity.note.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
 
-    List<Note> findAllByUserId(String userId);
+    Page<Note> findAllByUserId(String userId, Pageable pageable);
+
+    Page<Note> findAllByTagsContaining(Tag tag, Pageable pageable);
+
+    Map<String, Long> getWordStatsById(String id);
 
 }
